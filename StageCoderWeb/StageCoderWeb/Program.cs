5804c8e545cbae568e829962d3a317c4afe6e358
Jimmy Engström <jimmy@apeoholic.se>
2023-03-15 14:15:05 +0100

    Added mapping for gif

--- StageCoderWeb/StageCoderWeb/Program.cs
+++ StageCoderWeb/StageCoderWeb/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
 using StageCoderWeb;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".gif"] = "image/gif";
+
+builder.Services.Configure<StaticFileOptions>(options => {
+    options.ContentTypeProvider = provider;
+});
 await builder.Build().RunAsync();
 
